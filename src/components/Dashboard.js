1b4c902e1d0be9e2d08eea8e4875553255db267c
Samriddhi Tayal <samriddhitayal@gmail.com>
2024-01-23 16:07:15 +0530

    login functionality implemented

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,49 @@
 import Blog from './Blog';
-import blogData from '../data/blogs.json'
+// import blogData from '../data/blogs.json';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 function Dashboard() {
+	const [blogs, setBlogs] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const getUserBlogs = () => {
+		axios
+			.post('http://localhost:8000/user', {
+				author: '58e726d74b245e76333bdff',
+			})
+			.then((res) => {
+				setBlogs(res.data.data.blogs);
+				setLoading(false);
+			})
+			.catch((err) => {
+				alert('Error: ', err);
+				setLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		getUserBlogs();
+	}, []);
 	return (
 		<div>
 			<button>Add Blog</button>
 			<button>Logout</button>
 			<p>Display user blogs</p>
-      {blogData.map(blogData=><Blog blogData={blogData}/>)}
+
+			{loading ? (
+				<p>Blogs are loading</p>
+			) : Array.isArray(blogs) ? (
+				blogs.map((blog, index) => (
+					<div key={index}>
+						<Blog
+							content={blog.content}
+							title={blog.title}
+							author={blog.author}
+						/>
+					</div>
+				))
+			) : (
+				<p>No Blogs found</p>
+			)}
 		</div>
 	);
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,50 @@
+import {useState} from 'react';
+import axios from 'axios';
 function Login() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('')
+	const [loggedIn, setLoggedIn] = useState(false);
+	const emailInputHandler = (e) => {
+		setEmail(e.target.value);
+	};
+	const passwordHandler=(e)=>{
+		setPassword(e.target.value);
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		
+		axios
+		.post('http://localhost:8000/user/login', {
+			data :{
+				email, password
+			}
+		})
+		.then((res) =>{
+			setLoggedIn(res.data.success);
+			console.log(res.data.message);
+		})
+		.catch((err)=>{
+			alert("Error: ", err);
+		})
+	};
 	return (
 		<div>
 			<p>Login page</p>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<input
 					type='email'
 					placeholder='Email'
+					value = {email}
+					onChange={emailInputHandler}
 				/>
 				<input
 					type='password'
 					placeholder='Password'
+					vvalue= {password}
+					onChange={passwordHandler}
 				/>
         <button>Login</button>
+				{loggedIn?<p>Logged In successfully</p>:null}
 			</form>
 		</div>
 	);
